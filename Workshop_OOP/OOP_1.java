f17c82edbf5d5079c231bf42509a940136d33fd1
Santirat138 <pleumsantirat@gmail.com>
2023-08-20 23:44:12 +0700

    Create OOP_1.java

---
+++ Workshop_OOP/OOP_1.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+class OOP_1{
+    public static int power(int base, int pow){
+        int i;
+        int ans=1;
+        for(i=1;i<=pow;i++){
+            ans=ans*base;
+        }
+        return ans;
+    }
+    public static void main(String args[]){
+        int x;
+        int result;
+        Scanner input=new Scanner(System.in);
+        System.out.print("Enter x: ");
+        x=input.nextInt();
+        int pow4, pow3;
+        pow4=power(x, 4);
+        pow3=power(x, 3);
+        result=(3*pow4)+(2*pow3)-x+10;
+        System.out.printf("Answer is %d", result);
+    }
+}
