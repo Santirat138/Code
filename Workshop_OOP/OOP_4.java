bf12c6a308d9d422317b248411f7596745718774
Santirat138 <pleumsantirat@gmail.com>
2023-08-22 14:51:06 +0700

    Update OOP_4.java

--- Workshop_OOP/OOP_4.java
+++ Workshop_OOP/OOP_4.java
@@ -6,6 +6,7 @@ class splitBox{
     int extNum;
     int newNum;
     int i;
+    int sum;
     void setSize(int digitIn){
         digiSize=digitIn;
         arrayNum=new int[digiSize];
@@ -18,18 +19,28 @@ class splitBox{
             System.out.printf("Number : %d\n", arrayNum[i]);
         }
     }
+    int sumNum(){
+        sum=0;
+        for(i=0;i<digiSize;i++){
+            sum=sum+arrayNum[i];
+            System.out.printf("Number : %d\n", arrayNum[i]);
+        }
+        return sum;
+    }
 }
 class OOP_4{
     public static void main(String[] args){
         Scanner input=new Scanner(System.in);
-        int num, digit;
+        int num, digit, result;
         System.out.print("Enter number: ");
         num=input.nextInt();
         digit=countDigits(num);
         splitBox box1=new splitBox();
         box1.setSize(digit);
         box1.newNum=num;
+        result=box1.sumNum();
         box1.splitNum();
+        System.out.printf("Sum of number %d is %d\n", num, result);
     }
     public static int countDigits(int numIn){
         int digi=0;
