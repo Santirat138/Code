b53eaf2afff9fec3785460e57b0429e1a0e120dc
Santirat138 <pleumsantirat@gmail.com>
2023-08-30 04:49:02 +0700

    Update Test_Workshop.cpp

--- Workshop_Data_Structure/Test_Workshop.cpp
+++ Workshop_Data_Structure/Test_Workshop.cpp
@@ -1,9 +1,9 @@
-    // u for push number in to stack.|
-    // o for pop number.|
-    // t for show top number.|
-    // p for sort first to last input.|
-    // n for show amount of number in stack.|
-    // x for end program.
+    // e for enqueue number in to queue.|
+    // d for dequeue number.|
+    // p for show the first to last number.
+    // n for show amount of number in queue.|
+    // s for show the first and the last number in queue.|
+    // x for end program.|
 #include<iostream>
 using namespace std;
 class node{
@@ -11,54 +11,58 @@ public:
     int num;
     node *next;
 };
+node *first=NULL;
+node **firstR=&first;
+node *last=NULL;
+node **lastR=&last;
 int amount=0;
-node *top=NULL;
-node **topR=&top;
-void push(int newNum);
-int pop();
-void showTop();
-void showSort();
+void enqueue(int newNum);
+int dequeue();
+void showAll();
 int main(){
     char chIn;
     int numIn;
     int numOut;
     do{
-        cout<<"Enter charIn : ";
+        cout<<"Enter char : ";
         cin>>chIn;
-        if(chIn=='u'){
+        if(chIn=='e'){
             cout<<"Enter number : ";
             cin>>numIn;
-            push(numIn);
         }
-        else if(chIn=='o'){
-            numOut=pop();
+        else if(chIn=='d'){
+            numOut=dequeue();
             cout<<numOut<<endl;
         }
-        else if(chIn=='t'){
-            showTop();
-        }
         else if(chIn=='p'){
-            showSort();
+            showAll();
         }
         else if(chIn=='n'){
             cout<<amount<<endl;
         }
+        else if(chIn=='s'){
+            cout<<"The first is "<<(*firstR)->num<<endl;
+            cout<<"The last is "<<(*lastR)->num<<endl;
+        }
     }
     while(chIn!='x');
 }
-void push(int newNum){
+void enqueue(int newNum){
     node *newNode=new node();
     newNode->num=newNum;
-    if(*topR!=NULL){
-        newNode->next=*topR;
+    if(*firstR!=NULL){
+        (*lastR)->next=newNode;
     }
-    *topR=newNode;
+    else{
+        *firstR=newNode;
+    }
+    *lastR=newNode;
     amount++;
 }
-int pop(){
-    if(*topR!=NULL){
-        node *temp=*topR;
-        *topR=(*topR)->next;
+int dequeue(){
+    if(*firstR!=NULL){
+        node *temp=*firstR;
+        *firstR=(*firstR)->next;
         temp->next=NULL;
         return temp->num;
     }
@@ -66,17 +70,10 @@ int pop(){
         return -1;
     }
 }
-void showTop(){
-    cout<<(*topR)->num<<endl;
-}
-void showSort(){
-    int array[amount];
-    int i;
-    for(i=0;i<amount;i++){
-        array[i]=pop();
+void showAll(){
+    node *show=*firstR;
+    while(show!=NULL){
+        cout<<show->num<<" ";
+        show=show->next;
     }
-    for(i=amount-1;i>=0;i--){
-        cout<<array[i]<<" ";
-    }
-    cout<<endl;
 }
