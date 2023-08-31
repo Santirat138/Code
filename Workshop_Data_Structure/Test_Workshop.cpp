38d911b980edd8027e9d575f705127ac7ad828d1
Santirat138 <pleumsantirat@gmail.com>
2023-08-31 11:23:41 +0700

    Update Test_Workshop.cpp

--- Workshop_Data_Structure/Test_Workshop.cpp
+++ Workshop_Data_Structure/Test_Workshop.cpp
@@ -1,79 +1,27 @@
-    // e for enqueue number in to queue.|
-    // d for dequeue number.|
-    // p for show the first to last number.
-    // n for show amount of number in queue.|
-    // s for show the first and the last number in queue.|
-    // x for end program.|
 #include<iostream>
 using namespace std;
 class node{
-public:
-    int num;
-    node *next;
+    public:
+        int num;
+        node *next;
 };
-node *first=NULL;
-node **firstR=&first;
-node *last=NULL;
-node **lastR=&last;
-int amount=0;
-void enqueue(int newNum);
-int dequeue();
-void showAll();
+node *head=NULL;
+node **headR=&head;
+
 int main(){
     char chIn;
-    int numIn;
-    int numOut;
     do{
         cout<<"Enter char : ";
         cin>>chIn;
-        if(chIn=='e'){
-            cout<<"Enter number : ";
-            cin>>numIn;
+        if(chIn=='a'){
+            // Insert after number.
+        }
+        else if(chIn=='i'){
+            // Insert before number.
         }
         else if(chIn=='d'){
-            numOut=dequeue();
-            cout<<numOut<<endl;
-        }
-        else if(chIn=='p'){
-            showAll();
-        }
-        else if(chIn=='n'){
-            cout<<amount<<endl;
-        }
-        else if(chIn=='s'){
-            cout<<"The first is "<<(*firstR)->num<<endl;
-            cout<<"The last is "<<(*lastR)->num<<endl;
+            // Delete number.
         }
     }
-    while(chIn!='x');
-}
-void enqueue(int newNum){
-    node *newNode=new node();
-    newNode->num=newNum;
-    if(*firstR!=NULL){
-        (*lastR)->next=newNode;
-    }
-    else{
-        *firstR=newNode;
-    }
-    *lastR=newNode;
-    amount++;
-}
-int dequeue(){
-    if(*firstR!=NULL){
-        node *temp=*firstR;
-        *firstR=(*firstR)->next;
-        temp->next=NULL;
-        return temp->num;
-    }
-    else{
-        return -1;
-    }
-}
-void showAll(){
-    node *show=*firstR;
-    while(show!=NULL){
-        cout<<show->num<<" ";
-        show=show->next;
-    }
+    while(chIn!='e');
 }
