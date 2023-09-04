0cb30b54eed1ca8ca668d20bb5199a06814b6d52
Santirat138 <pleumsantirat@gmail.com>
2023-09-04 17:08:48 +0700

    Update OOP_T2_7.java

--- Workshop_OOP/OOP_T2_7.java
+++ Workshop_OOP/OOP_T2_7.java
@@ -1,11 +1,13 @@
 import java.util.Scanner;
 public class OOP_T2_7{
     public static void main(String[] args){
+        boolean status;
         arrayBox boxObj=new arrayBox();
-        Scanner input=new Scanner(System.in);
         boxObj.setSize();
         boxObj.setElement();
         boxObj.showArray();
+        status=boxObj.check23();
+        System.out.println(status);
     }
 }
 class arrayBox{
@@ -24,6 +26,15 @@ class arrayBox{
             System.out.printf("Enter index%d: ", i);
             array[i]=input.nextInt();
         }
+    }
+    public boolean check23(){
+        boolean status=true;
+        for(i=0;i<size;i++){
+            if((array[i]==2)||(array[i]==3)){
+                status=false;
+            }
+        }
+        return status;
     }
     public void showArray(){
         for(i=0;i<size;i++){
