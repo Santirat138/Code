cc045d9bda547f0a0058b667ee08ee5ea2d512dc
Santirat138 <pleumsantirat@gmail.com>
2023-09-07 12:33:42 +0700

    Update Full_Link_List_Ver_2.cpp

--- My_Project/Full_Link_List_Ver_2.cpp
+++ My_Project/Full_Link_List_Ver_2.cpp
@@ -3,53 +3,120 @@ using namespace std;
 class node{
     public:
         int num;
+        node *prev;
         node *next;
 };
 class info{
     public:
+        float avg;
+        int sum;
         int amount=0;
-        
-        int calSum(node **headRef){
+        void calSum(node **headRef){
             node *currNode=*headRef;
-            int sum=0;
+            sum=0;
             while(currNode!=NULL){
                 sum=sum+currNode->num;
                 currNode=currNode->next;
             }
-            return sum;
         }
+        void calAvg(){
+            avg=sum/amount;
+        }
+
 };
 info data;
-void addHead(node **headRef, int newNum);
-void show(node **headRef);
+bool isSameNum(node **headRef, int checkNum);
+void connectNode(node *node1, node *node2);
+void addHead(node **headRef, node **tailRef, int newNum);
+void show(node **headRef, node **tailRef);
+node *findNum(node **headRef, int target);
 //------------------ main ------------------
 int main(){
     node *head=NULL;
     node **headR=&head;
+    node *tail=NULL;
+    node **tailR=&tail;
     info infoNode;
     int i;
     for(i=0;i<=5;i++){
-        addHead(headR, i);
+        addHead(headR, tailR, i);
     }
-    show(headR);
-    int totalNum=data.calSum(headR);
+    show(headR, tailR);
+    data.calSum(headR);
+    int totalNum=data.sum;
     cout<<totalNum;
 }
 //------------------ main ------------------
-void addHead(node **headRef, int newNum){
-    node *newNode=new node();
-    newNode->num=newNum;
-    if(*headRef!=NULL){
-        newNode->next=*headRef;
-    }
-    *headRef=newNode;
-    data.amount++;
+void connectNode(node *node1, node *node2){
+    node1->next=node2;
+    node2->prev=node1;
 }
-void show(node **headRef){
-    node *showNode=*headRef;
-    while(showNode!=NULL){
-        cout<<showNode->num<<" ";
-        showNode=showNode->next;
+void addHead(node **headRef, node **tailRef, int newNum){
+    if(!isSameNum(headRef, newNum)){
+        node *newNode=new node();
+        newNode->num=newNum;
+        if((*headRef!=NULL)&&(*tailRef!=NULL)){
+            connectNode(newNode, *headRef);
+        }
+        else if((*headRef==NULL)||(*tailRef==NULL)){
+            *tailRef=newNode;
+        }
+        *headRef=newNode;
+        data.amount++;
+    }
+    else{
+        cout<<"Can't add "<<newNum<<endl;
+    }
+}
+void show(node **headRef, node **tailRef){
+    node *startNode=*headRef;
+    node *endNode=*tailRef;
+    cout<<"Head --> Tail."<<endl;
+    while(startNode!=NULL){
+        cout<<startNode->num<<" ";
+        startNode=startNode->next;
+    }
+    cout<<endl<<endl<<"Tail --> Head."<<endl;
+    while(endNode!=NULL){
+        cout<<endNode->num<<" ";
+        endNode=endNode->prev;
     }
     cout<<endl;
+}
+bool isSameNum(node **headRef, int checkNum){
+    bool status=false;
+    if(*headRef!=NULL){
+        node *checkNode=*headRef;
+        while(checkNode!=NULL){
+            if(checkNode->num==checkNum){
+                status=true;
+                break;
+            }
+            else{
+                checkNode=checkNode->next;
+            }
+        }
+        return status;
+    }
+    else if(*headRef==NULL){
+        return status;
+    }
+}
+node *findNum(node **headRef, int target){
+    if(*headRef!=NULL){
+        node *currNode=*headRef;
+        while(currNode!=NULL){
+            if(currNode->num==target){
+                break;
+            }
+            else{
+                currNode=currNode->next;
+            }
+        }
+        return currNode;
+    }
+    else{
+        cout<<"Not found."<<endl;
+        return NULL;
+    }
 }
