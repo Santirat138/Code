2a8f5e9471e22776550e7ea34f9e6d1fc4d5cd98
Santirat138 <pleumsantirat@gmail.com>
2023-09-13 23:23:26 +0700

    Update OOP_Node.cpp

---
+++ My_Project/OOP_Node.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+using namespace std;
+class node{
+    public:
+        int num;
+        node *prev;
+        node *next;
+};
+
+int main(){
+    node *head=NULL;
+    node **headR=&head;
+    node *tail=NULL;
+    node **tailR=&tail;
+    
+    
+}
