bfa33a285bd598818376a8ae6f6fd8821578cfd7
Santirat138 <pleumsantirat@gmail.com>
2023-09-25 15:48:30 +0700

    Update Untitled-1.java

--- Workshop_OOP/Untitled-1.java
+++ Workshop_OOP/Untitled-1.java
@@ -1,3 +1,15 @@
+import java.util.Scanner;
 public class Untitled-1 {
-    
+    public static void main(String[] args){
+        Scanner input=new Scanner(System.in);
+        bigBox bBox=new bigBox(10);
+
+    }
+}
+class bigBox{
+    public int num;
+
+    public bigBox(int newNum){
+        num=newNum;
+    }
 }
