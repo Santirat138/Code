c029732fc3f31480c524a898cf03ffa509eee4af
Santirat138 <pleumsantirat@gmail.com>
2023-09-25 08:28:18 +0700

    Update Warmup.cpp

--- Workshop_Data_Structure/Warmup.cpp
+++ Workshop_Data_Structure/Warmup.cpp
@@ -1,3 +1,4 @@
+// Stack
 #include<iostream>
 using namespace std;
 class node{
@@ -5,50 +6,22 @@ class node{
         int num;
         node *next;
 };
-void addFirst(node **headRef, int newNum);
-void show(node **headRef);
-node *findNode(node **headRef, int target);
-void deleteNode(node **headRef, int target);
-/*-----------------------------------------*/ 
+void push(node **topRef, int newNum);
+int pop(node **topRef);
 int main(){
-    node *head=NULL;
-    node **headR=&head;
-    for(int i=0;i<=5;i++){
-        addFirst(headR, i);
-    }
+    node *top=NULL;
+    node **topRef=NULL;
 
 }
-/*-----------------------------------------*/ 
-void addFirst(node **headRef, int newNum){
+void push(node **topRef, int newNum){
     node *newNode=new node();
-    if(*headRef==NULL){
-        *headRef=newNode;
+    newNode->num=newNum;
+    if(*topRef!=NULL){
+        newNode->next=*topRef;
     }
-    else if(*headRef!=NULL){
-        newNode->next=*headRef;
-        *headRef=newNode;
-    }
+    *topRef=newNode;
 }
-void show(node **headRef){
-    node *showNode=*headRef;
-    while(showNode!=NULL){
-        cout<<showNode->num<<" ";
-        showNode=showNode->next;
-    }
-    cout<<endl<<endl;
-}
-node *findNode(node **headRef, int target){
-    node *currNode=*headRef;
-    while(currNode!=NULL){
-        if(currNode->num==target){
-            break;
-        }
-        else{
-            currNode=currNode->next;
-        }
-    }
-    return currNode;
-}
-void deleteNode(node **headRef, int target){
-
+int pop(node **topRef){
+    node *tempNode;
+    
 }
