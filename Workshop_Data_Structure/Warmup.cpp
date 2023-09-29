37fb411a2419fd3bcd0a356c82387bda9c4048db
Santirat138 <pleumsantirat@gmail.com>
2023-09-29 18:13:48 +0700

    Update Warmup.cpp

--- Workshop_Data_Structure/Warmup.cpp
+++ Workshop_Data_Structure/Warmup.cpp
@@ -1,27 +1,6 @@
-// Stack
 #include<iostream>
 using namespace std;
-class node{
-    public:
-        int num;
-        node *next;
-};
-void push(node **topRef, int newNum);
-int pop(node **topRef);
-int main(){
-    node *top=NULL;
-    node **topRef=NULL;
 
-}
-void push(node **topRef, int newNum){
-    node *newNode=new node();
-    newNode->num=newNum;
-    if(*topRef!=NULL){
-        newNode->next=*topRef;
-    }
-    *topRef=newNode;
-}
-int pop(node **topRef){
-    node *tempNode;
+int main(){
     
 }
