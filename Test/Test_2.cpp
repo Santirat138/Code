ca1bda2146527e9f1a7025df38442009a94d4d80
Santirat138 <pleumsantirat@gmail.com>
2023-10-12 15:19:31 +0700

    Update Test_2.cpp

--- Test/Test_2.cpp
+++ Test/Test_2.cpp
@@ -1,26 +1,11 @@
-// Static method
+// Test function recursive show number in node.
 #include<iostream>
 using namespace std;
-class numBox{
+class node{
     public:
-        static int num;
-
-        numBox(){
-            cout<<"Call"<<endl;
-            num++;
-        }
-        static int getNum(){
-            return num;
-        }
-        static void numUp(){
-            num=num*10;
-        }
+        int num;
+        node *next;
 };
-int numBox::num=2;
 int main(){
-    cout<<"Start value "<<numBox::getNum();
-    numBox box1;
-    numBox box2;
-    box1.numUp();
-    cout<<"Total value "<<numBox::getNum();
+
 }
