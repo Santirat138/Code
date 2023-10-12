f46b7935631b56b17c58face7ca4f9149091932e
Santirat138 <pleumsantirat@gmail.com>
2023-10-12 15:19:34 +0700

    Update Test_1.cpp

--- Test/Test_1.cpp
+++ Test/Test_1.cpp
@@ -1,33 +1,10 @@
-// Static member
+// Function recursive power
 #include<iostream>
 using namespace std;
-class numBox{
-    public:
-        static int round;
-        static int val;
-
-        numBox(){
-            round++;
-            val=val+10;
-        }
-        void setNum(int newVal){
-            val=newVal;
-        }
-        void showData(){
-            cout<<"Round "<<round<<endl;
-            cout<<"Value "<<val<<endl<<endl;
-        }
-};
-int numBox::round=0;
-int numBox::val=100;
+int powRe(int base, int pow);
 int main(){
-    numBox box1;
-    box1.showData();
 
-    numBox box3;
-    box3.setNum(900);
-    box3.showData();
-     
-    numBox box2;
-    box2.showData();
+}
+int powRe(int base, int pow){
+    
 }
