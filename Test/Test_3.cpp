9c134dffef6fc49edc16acd4e150f2ce5284b295
Santirat138 <pleumsantirat@gmail.com>
2023-10-12 15:19:29 +0700

    Create Test_3.cpp

---
+++ Test/Test_3.cpp
@@ -0,0 +1,17 @@
+// Method create node.
+#include<iostream>
+using namespace std;
+class node{
+public:
+    int num;
+    node *next;
+
+    // Method add before head;
+    node(int newNum){
+        num=newNum;
+        next=NULL;
+    }
+};
+int main(){
+
+}
